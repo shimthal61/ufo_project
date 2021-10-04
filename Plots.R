47a8cd7ea51c886aa7e693d2a98100bcab53c7f8
Harvey Schneider <50023408+shimthal61@users.noreply.github.com>
2021-10-04 16:11:59 +0100

    Creating the first plot examining the top 10 states for UFO sightings

---
+++ Plots.R
@@ -0,0 +1,20 @@
+# Read in data
+ufo_sightings <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-06-25/ufo_sightings.csv")
+
+# Remove missing data points
+plot1 <- ufo_sightings %>% 
+  filter(!is.na(state)) %>%
+  mutate(state = str_to_upper(state)) %>%
+  group_by(state) %>%
+  tally() %>%
+  top_n(10) %>%
+  ggplot(aes(x = reorder(state, n), y = n, fill = state)) + 
+  geom_col() + 
+  coord_flip() +
+  guides(fill = "none") + 
+  labs(title = "Top 10 States for Ufo Sightings",
+       x = NULL,
+       y = NULL) +
+  ylim(0, 11000) +
+  theme_minimal() +
+  theme(text = element_text(size = 15))
